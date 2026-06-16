a4138cee116ff2b52904abc8ba57653b51443246
Michel Soares <michel_soares@outlook.com>
2026-06-15 21:19:56 -0300

    refactor: remove dependência aspnetcore da camada de Application (#164)

--- src/Application/Exceptions/ServiceValidationException.cs
+++ src/Application/Exceptions/ServiceValidationException.cs
@@ -1,9 +1,9 @@
-using Microsoft.AspNetCore.Http;
+using System.Net;
 
 namespace Fiap.TechChallenge.OficinaMecanica.Application.Exceptions;
 
 public sealed class ServiceValidationException : InvalidOperationException, IServiceExceptionContract
 {
     public ServiceValidationException(string message) : base(message) { }
-    public int StatusCode => StatusCodes.Status400BadRequest;
+    public int StatusCode => (int)HttpStatusCode.BadRequest;
 }
