f46ed737ef5a3acc6b229a2916430cd0b93de822
Michel de Lima Soares <michel_soares@outlook.com>
2026-08-17 19:31:13 -0300

    feat: add cliente status - solicitado no TC fase 3

---
+++ src/Infrastructure/Migrations/20260817221356_AddStatusCliente.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fiap.TechChallenge.OficinaMecanica.Api.src.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStatusCliente : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "Cliente",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "Ativo");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cliente_Status",
+                table: "Cliente",
+                column: "Status");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Cliente_Status",
+                table: "Cliente");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Cliente");
+        }
+    }
+}
